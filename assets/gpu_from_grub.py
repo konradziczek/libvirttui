5a941e14908492613f8917af95070445b5215938
Konrad Zaworski <konrad@zaworski.pl>
2025-05-26 23:58:13 +0200

    extract gpu

--- assets/gpu_from_grub.py
+++ assets/gpu_from_grub.py
@@ -2,6 +2,8 @@ import os
 import sys
 import re
 import json
+import pwd
+import grp
 
 with open('/etc/default/grub', 'r') as f:
     content = f.read()
@@ -19,8 +21,12 @@ print(f"Found: {pci_ids}")
 if not os.path.exists('/opt/libvirttui/gpu.json'):
     with open('/opt/libvirttui/gpu.json', 'w') as json_file:
         json.dump(pci_ids, json_file, indent=2)
+        print("File saved.")
+    if os.geteuid() == 0:
+        print("Changing owner and group to libvirttui.")
+        uid = pwd.getpwnam('libvirttui').pw_uid
+        gid = grp.getgrnam('libvirttui').gr_gid
+        os.chown('/opt/libvirttui/gpu.json', uid, gid)
 else:
     print("File /opt/libvirttui/gpu.json already exists - it will not be replaced.")
     sys.exit(1)
-
-print(f"Done.")
