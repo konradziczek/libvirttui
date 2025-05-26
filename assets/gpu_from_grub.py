ee7d774fd4aae6e252c2de323184637888378492
Konrad Zaworski <konrad@zaworski.pl>
2025-05-26 23:50:21 +0200

    extract gpu

---
+++ assets/gpu_from_grub.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import re
+import json
+
+with open('/etc/default/grub', 'r') as f:
+    content = f.read()
+
+match = re.search(r'pci-stub\.ids=([^\s"]+)', content)
+if not match:
+    print("Parameter pci-stub.ids not found in /etc/default/grub.")
+    sys.exit(1)
+
+pci_ids_str = match.group(1)
+pci_ids = pci_ids_str.split(',')
+
+print(f"Found: {pci_ids}")
+
+if not os.path.exists('/opt/libvirttui/gpu.json'):
+    with open('/opt/libvirttui/gpu.json', 'w') as json_file:
+        json.dump(pci_ids, json_file, indent=2)
+else:
+    print("File /opt/libvirttui/gpu.json already exists - it will not be replaced.")
+    sys.exit(1)
+
+print(f"Done.")
